35157be1464bc5c0d94b61855f2f264f555679c0
武文韬 <winter_wu@sjtu.edu.cn>
2018-04-04 21:37:44 +0800

    Add files via upload

--- VMcompiler/CodeWriter.java
+++ VMcompiler/CodeWriter.java
@@ -13,6 +13,8 @@ public class CodeWriter {
     private String LINE_SEPARATOR = System.getProperty("line.separator");
     private int seq = -1;
     private static final String LABEL_PATTEN3 = "{0}.{1}.{2}";//模板
+    private static final String LABEL_PATTEN2 = "{0}.{1}";
+    public static final String LABEL_PATTEN1 = "{0}${1}";
 
     public void setPath(String filePath) {
         this.filePath = filePath.endsWith(File.separator) ? filePath : filePath
@@ -34,11 +36,11 @@ public class CodeWriter {
     }
 
     public void writeArithmetic(String command) {
-        StringBuffer strBuf = new StringBuffer();
+        StringBuilder strBuf = new StringBuilder();
         String lab1, lab2, lab3, result;
 
-        if (command.equals("add") || command.equals("sub") ||
-                command.equals("and") || command.equals("or")) {
+        if (command.equalsIgnoreCase("add") || command.equalsIgnoreCase("sub") ||
+                command.equalsIgnoreCase("and") || command.equalsIgnoreCase("or")) {
             strBuf.append("@").append("SP").append(LINE_SEPARATOR)
                     .append("AM=M-1").append(LINE_SEPARATOR).append("D=M")
                     .append(LINE_SEPARATOR).append("@").append("SP")
@@ -47,15 +49,15 @@ public class CodeWriter {
                     .append(LINE_SEPARATOR).append("@").append("SP")
                     .append(LINE_SEPARATOR).append("M=M+1")
                     .append(LINE_SEPARATOR);
-        } else if (command.equals("eq") || command.equals("gt") ||
-                command.equals("lt")) {
+        } else if (command.equalsIgnoreCase("eq") || command.equalsIgnoreCase("gt") ||
+                command.equalsIgnoreCase("lt")) {
             ++seq;
-            lab1 = MessageFormat.format(LABEL_PATTEN3, new Object[] { "COMP",
-                    seq, "TRUE" });
-            lab2 = MessageFormat.format(LABEL_PATTEN3, new Object[] { "COMP",
-                    seq, "FALSE" });
-            lab3 = MessageFormat.format(LABEL_PATTEN3, new Object[] { "COMP",
-                    seq, "END" });
+            lab1 = MessageFormat.format(LABEL_PATTEN3, "COMP",
+                    seq, "TRUE");
+            lab2 = MessageFormat.format(LABEL_PATTEN3, "COMP",
+                    seq, "FALSE");
+            lab3 = MessageFormat.format(LABEL_PATTEN3, "COMP",
+                    seq, "END");
 
             strBuf.append("@").append("SP").append(LINE_SEPARATOR)
                     .append("AM=M-1").append(LINE_SEPARATOR).append("D=M")
@@ -130,7 +132,125 @@ public class CodeWriter {
     }
 
     public void writePushPop(String command, String segment, int index) {
+        StringBuilder strBuf = new StringBuilder();
 
+        if (command.equalsIgnoreCase("push")) {
+            if (segment.equalsIgnoreCase("constant")) {
+                strBuf.append("@").append(index).append(LINE_SEPARATOR)
+                        .append("D=A").append(LINE_SEPARATOR).append("@")
+                        .append("SP").append(LINE_SEPARATOR).append("A=M")
+                        .append(LINE_SEPARATOR).append("M=D")
+                        .append(LINE_SEPARATOR).append("@").append("SP")
+                        .append(LINE_SEPARATOR).append("M=M+1")
+                        .append(LINE_SEPARATOR);
+            } else if (segment.equalsIgnoreCase("local")
+                    || segment.equalsIgnoreCase("argument")
+                    || segment.equalsIgnoreCase("this")
+                    || segment.equalsIgnoreCase("that")) {
+
+                String seg = judge(segment);
+
+                strBuf.append("@").append(index).append(LINE_SEPARATOR)
+                        .append("D=A").append(LINE_SEPARATOR).append("@")
+                        .append(seg).append(LINE_SEPARATOR).append("A=M")
+                        .append(LINE_SEPARATOR).append("D=D+A")
+                        .append(LINE_SEPARATOR).append("A=D")
+                        .append(LINE_SEPARATOR).append("D=M")
+                        .append(LINE_SEPARATOR).append("@").append("SP")
+                        .append(LINE_SEPARATOR).append("A=M")
+                        .append(LINE_SEPARATOR).append("M=D")
+                        .append(LINE_SEPARATOR).append("@").append("SP")
+                        .append(LINE_SEPARATOR).append("M=M+1")
+                        .append(LINE_SEPARATOR);
+            } else if (segment.equalsIgnoreCase("temp")
+                    || segment.equalsIgnoreCase("pointer")
+                    || segment.equalsIgnoreCase("static")) {
+                String var = MessageFormat.format(LABEL_PATTEN2, asmName, index);
+                int base = 5;
+                if (segment.equalsIgnoreCase("pointer")) {
+                    base = 3;
+                }
+
+                String varStr = segment.equalsIgnoreCase("static") ? var : "R"
+                        + (base + index);
+
+                strBuf.append("@").append(varStr).append(LINE_SEPARATOR)
+                        .append("D=M").append(LINE_SEPARATOR).append("@")
+                        .append("SP").append(LINE_SEPARATOR).append("A=M")
+                        .append(LINE_SEPARATOR).append("M=D")
+                        .append(LINE_SEPARATOR).append("@").append("SP")
+                        .append(LINE_SEPARATOR).append("M=M+1")
+                        .append(LINE_SEPARATOR);
+            }
+        } else if (command.equalsIgnoreCase("pop")) {
+                if (segment.equalsIgnoreCase("local")
+                        || segment.equalsIgnoreCase("argument")
+                        || segment.equalsIgnoreCase("this")
+                        || segment.equalsIgnoreCase("that")) {
+
+                    String seg = judge(segment);
+
+
+                    strBuf.append("@").append(index).append(LINE_SEPARATOR)
+                            .append("D=A").append(LINE_SEPARATOR).append("@")
+                            .append(seg).append(LINE_SEPARATOR).append("A=M")
+                            .append(LINE_SEPARATOR).append("D=D+A")
+                            .append(LINE_SEPARATOR).append("@").append(seg)
+                            .append(LINE_SEPARATOR).append("M=D")
+                            .append(LINE_SEPARATOR).append("@").append("SP")
+                            .append(LINE_SEPARATOR).append("AM=M-1")
+                            .append(LINE_SEPARATOR).append("D=M")
+                            .append(LINE_SEPARATOR).append("@").append(seg)
+                            .append(LINE_SEPARATOR).append("A=M")
+                            .append(LINE_SEPARATOR).append("M=D")
+                            .append(LINE_SEPARATOR).append("@").append(index)
+                            .append(LINE_SEPARATOR).append("D=A")
+                            .append(LINE_SEPARATOR).append("@").append(seg)
+                            .append(LINE_SEPARATOR).append("A=M")
+                            .append(LINE_SEPARATOR).append("D=A-D")
+                            .append(LINE_SEPARATOR).append("@").append(seg)
+                            .append(LINE_SEPARATOR).append("M=D")
+                            .append(LINE_SEPARATOR);
+                }
+
+                if (segment.equalsIgnoreCase("temp")
+                        || segment.equalsIgnoreCase("pointer")
+                        || segment.equalsIgnoreCase("static")) {
+                    String var = MessageFormat.format(LABEL_PATTEN2, asmName, index);
+                    int base = 5;
+                    if (segment.equalsIgnoreCase("pointer")) {
+                        base = 3;
+                    }
+
+                    String varStr = segment.equalsIgnoreCase("static") ? var : "R"
+                            + (base + index);
+
+                    strBuf.append("@").append("SP").append(LINE_SEPARATOR)
+                            .append("AM=M-1").append(LINE_SEPARATOR).append("D=M")
+                            .append(LINE_SEPARATOR).append("@")
+                            .append(varStr)
+                            .append(LINE_SEPARATOR).append("M=D")
+                            .append(LINE_SEPARATOR);
+                }
+            }
+
+        try {
+            fw.write(strBuf.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+
+    private String judge(String segment) {
+        if (segment.equalsIgnoreCase("local")) {
+            return "LCL";
+        } else if (segment.equalsIgnoreCase("argument")) {
+            return "ARG";
+        } else if (segment.equalsIgnoreCase("this")) {
+            return "THIS";
+        } else {
+            return "THAT";
+        }
     }
 
     public void close() {
